22cd56cd4aeead6a9a20f20baece588a40a8520e
sh95fit <h95fit@gmail.com>
2023-02-27 16:31:29 +0900

    Django REST framework(기본 제공 Auth_User와 authenticate를 활용해 로그인 구현)

--- Django_API/REST_API/restapi_test/views.py
+++ Django_API/REST_API/restapi_test/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from restapi_test.models import Addresses
 from restapi_test.serializers import AddressesSerializer
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
@@ -49,15 +50,34 @@ def addresses(request, pk):
         return HttpResponse(status=204)
 
 
+# @csrf_exempt
+# def login(request):
+#     if request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         search_name = data["name"]
+#         obj = Addresses.objects.get(name=search_name)
+#         print(obj.phone_number)
+
+#         if data["phone_number"] == obj.phone_number:
+#             return HttpResponse(status=200)
+#         else:
+#             return HttpResponse(status=400)
+
 @csrf_exempt
 def login(request):
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        search_name = data["name"]
-        obj = Addresses.objects.get(name=search_name)
-        print(obj.phone_number)
 
-        if data["phone_number"] == obj.phone_number:
+    if request.method == "POST":
+        print("request log :" + str(request.body))
+        id = request.POST.get('userid', '')
+        pw = request.POST.get('userpw', '')
+        print("input id = " + id + " / input pw = " + pw)
+
+        result = authenticate(username=id, password=pw)
+
+        if result:
+            print("로그인 성공")
             return HttpResponse(status=200)
         else:
-            return HttpResponse(status=400)
+            print("로그인 실패")
+            return HttpResponse(status=401)
+    return render(request, 'login.html')
