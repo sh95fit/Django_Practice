498c4c7de1b67eb2446f9289b757f617a2f8dc82
sh95fit <h95fit@gmail.com>
2023-02-28 15:13:19 +0900

    Django 기본 제공 기능 활용 - 회원가입 구현

--- Django_TEST/API_TEST/users/views.py
+++ Django_TEST/API_TEST/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, JsonResponse
+from .models import User
 
 # Create your views here.
 def login_view(request) :
@@ -21,3 +22,22 @@ def login_view(request) :
 def logout_view(request) :
     logout(request)
     return redirect("user:login")
+
+def signup_view(request) :
+    
+    if request.method == "POST" :
+        print(request.POST)
+        username = request.POST["username"]
+        password = request.POST["password"]
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        email = request.POST["email"]
+        pwd_id = request.POST["pwd_id"]
+        
+        user = User.objects.create_user(username, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.pwd_id = pwd_id
+        user.save()
+        return redirect("user:login")
+    return render(request, "users/signup.html")
