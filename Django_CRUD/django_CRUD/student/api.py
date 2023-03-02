0ac2a7bb205484eeb06f3a07d4b940207af32987
sh95fit <h95fit@gmail.com>
2023-03-02 16:27:53 +0900

    Django CRUD - REST API POST(CREATE) 구현(+DB에 데이터 저장)

--- Django_CRUD/django_CRUD/student/api.py
+++ Django_CRUD/django_CRUD/student/api.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import *
+from rest_framework import status
 
 class StudentList(APIView) :
     
@@ -8,3 +9,14 @@ class StudentList(APIView) :
         model = Student.objects.all()
         serializer = StudentSerializer(model, many=True)
         return Response(serializer.data)
+    
+    def post(self, request) :
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid() :
+            student = Student()
+            student.student_id = request.data['student_id']
+            student.name = request.data['name']
+            student.age = request.data['age']
+            student.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
