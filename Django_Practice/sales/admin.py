7384070aec510cc5466e37bda5c6617b2d6b873b
sh95fit <h95fit@gmail.com>
2024-09-04 10:59:54 +0900

    Django 리프레시 : View를 활용한 Template 데이터 표시 복습

--- Django_Practice/sales/admin.py
+++ Django_Practice/sales/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import CustomID, Sale, Person
 # Register your models here.
+admin.site.register(CustomID)
+admin.site.register(Sale)
+admin.site.register(Person)
--- Django_Practice/sales/views.py
+++ Django_Practice/sales/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import CustomID
+
+
+def Homepage(request):
+    customer = CustomID.objects.all()
+    context = {
+        "MenuName": "Noodle",
+        "Price": "10,000 won",
+        "Customer": customer
+    }
+    return render(request, "sales/index.html", context)
