f93d7badaa36b09273cd83e18003ff44a53edb6d
sh95fit <h95fit@gmail.com>
2024-09-04 13:40:52 +0900

    Django 리프레시 - 일반 폼을 이용한 DB Input 복습

--- Django_Practice/sales/urls.py
+++ Django_Practice/sales/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 
-from .views import Homepage, SalesList, SalesDetail
+from .views import Homepage, SalesList, SalesDetail, SaleInput
 
 app_name = "Sales"
 
 urlpatterns = [
     path('', Homepage),
     path('sales', SalesList),
-    path('sales/<int:pk>', SalesDetail)
+    path('sales/<int:pk>', SalesDetail),
+    path('sales/make', SaleInput),
 ]
--- Django_Practice/sales/views.py
+++ Django_Practice/sales/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .models import CustomID, Sale
+from .models import CustomID, Sale, Person
+
+from .forms import SaleInputForm
 
 
 def Homepage(request):
@@ -31,3 +33,36 @@ def SalesDetail(request, pk):
     }
     # return HttpResponse(f"Detail Page - {sale.first_name} {sale.last_name}")
     return render(request, "sales/sales_detail.html", detail_context)
+
+
+def SaleInput(request):
+    # print(request.POST)   # 폼에 의해 입력 후 전송된 정보 확인
+
+    # 유효성 확인되면 DB에 저장
+    form = SaleInputForm()
+
+    if request.method == "POST":
+        form = SaleInputForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)  # cleaned_data를 통해 필요한 정보들만 추려낼 수 있다
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            person = Person.objects.first()
+
+            Sale.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                person=person
+            )
+
+            print("정보가 정상 입력되었습니다.")
+
+            return redirect("/sales")
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "sales/sales_input.html", context)
