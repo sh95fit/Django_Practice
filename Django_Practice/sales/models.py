6fc912e065e88d0d013891d067e2666598350a1b
sh95fit <h95fit@gmail.com>
2024-09-04 10:08:59 +0900

    Django 리프레시 - Model 생성 및 데이터베이스 마이그레이션 복습

---
+++ Django_Practice/sales/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# 장고의 내장 유저 모델을 사용하고 싶은 경우 (간단한 방법)
+# from django.contrib.auth import get_user_model
+# ID = get_user_model()
+
+# 장고에서 내장 유저 모델을 사용하는 기본적인 방법
+# AbstractUser : 모델을 django에서 미리 지정해둬 간편하게 가져와 사용 가능
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomID (AbstractUser):
+    pass  # AbstractUser을 그대로 사용할 경우
+
+
+# 모델 생성 시 ID는 django에서 자동 생성!
+class Sale(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField(default=0)
+    person = models.ForeignKey("Person", on_delete=models.CASCADE)
+    # models.SET_NULL : Null로 변경하고 싶은 경우
+    # models.SET_DEFAULT : 삭제 시 기본값으로 지정하고 싶은 경우
+
+    # funnels_type = (
+    #     ('naver', '네이버'),
+    #     ('google', '구글'),
+    #     ('news', '뉴스레터'),
+    # )
+
+    # 다양한 타입 형태 존재
+    # is_existed = models.BooleanField(default=False)
+    # funnels = models.CharField(choices=funnels_type, max_length=200)
+    # your_images = models.ImageField(blank=True, null=True)
+    # your_file = models.FileField(blank=True, null=True)
+
+
+class Person(models.Model):
+    customer = models.OneToOneField(CustomID, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.customer.email
