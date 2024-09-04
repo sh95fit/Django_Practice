98f944b0cd4a02bdfa1b5cf1a6bef595f65aff8f
sh95fit <h95fit@gmail.com>
2024-09-04 16:44:33 +0900

    Django 리프레시 - static 파일 지정 복습

--- Django_Practice/djangopt/urls.py
+++ Django_Practice/djangopt/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("sales.urls", namespace="Sales"))
+    path('', include("sales.urls", namespace="Sales")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
---
+++ Django_Practice/static/js/main.js
@@ -0,0 +1,1 @@
+console.log("Include Test");
