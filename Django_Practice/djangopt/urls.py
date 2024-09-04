c42766bb104d5ec0a1201f22be82c1c7051533b0
sh95fit <h95fit@gmail.com>
2024-09-04 18:08:54 +0900

    Django 리프레시 - Django 내장 뷰 활용 Login, Logout 구현 복습

--- Django_Practice/djangopt/urls.py
+++ Django_Practice/djangopt/urls.py
@@ -4,9 +4,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth.views import LoginView, LogoutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("sales.urls", namespace="Sales")),
+    # 로그인 성공 시 기본 리다이렉트 경로가 accounts/profile로 되어 있어 수정 필요! -> settings.py에서 설정
+    path('login', LoginView.as_view(), name="Login"),
+    path('logout', LogoutView.as_view(), name="Logout")
 ]
 
 if settings.DEBUG:
