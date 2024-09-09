1081c6313abd06f4b1639e8010615aa5c7a48ec6
sh95fit <h95fit@gmail.com>
2024-09-09 11:20:46 +0900

    Django-React 연동 실습 - React 입력 폼 구성 및 Django POST API 적용 (by Fetch)

--- Django_React/djangomusic/frontjob/src/components/NewMakeRoomPage.js
+++ Django_React/djangomusic/frontjob/src/components/NewMakeRoomPage.js
@@ -1,12 +1,122 @@
 import React, { Component } from "react";
 
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+
 export default class NewMakeRoomPage extends Component{
+  defaultVotes = 1;
+
   constructor(props) {
     super(props);
+    this.state = {
+      guestCanPause: true,
+      votesToSkip: this.defaultVotes,
+    };
+
+    this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+    this.handleVotesChange = this.handleVotesChange.bind(this);
+    this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+  }
+
+  handleVotesChange(e){
+    this.setState({
+      votesToSkip:e.target.value,
+    });
+  }
+
+  handleGuestCanPauseChange(e){
+    this.setState({
+      guestCanPause:e.target.value === "true" ? true : false,
+    });
+  }
+
+  handleRoomButtonPressed(){
+    const requestOptions = {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        vote_to_skip: this.state.votesToSkip,
+        guest_can_pause: this.state.guestCanPause,
+      }),
+    };
+
+    fetch("/musicapi/create", requestOptions).then((response)=>response.json()).then((data)=>console.log(data));
   }
 
   render() {
-    return <h1>NewMakeRoomPage 입니다.</h1>
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography component="h4" variant="h4">
+          🎵음악 방 만들기
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl component="fieldset">
+            <FormHelperText>
+              방문자 재생 상태 제어
+            </FormHelperText>
+            <RadioGroup
+              row
+              defaultValue="true"
+              onChange={this.handleGuestCanPauseChange}
+            >
+              <FormControlLabel
+                value="true"
+                control={<Radio color="primary" />}
+                label="연주/중단"
+                labelPlacement="bottom"
+              />
+              <FormControlLabel
+                value="false"
+                control={<Radio color="secondary" />}
+                label="자동"
+                labelPlacement="bottom"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl>
+            <TextField
+              required={true}
+              type="number"
+              onChange={this.handleVotesChange}
+              defaultValue={this.defaultVotes}
+              inputProps={{
+                min: 1,
+                style: { textAlign: "center" },
+              }}
+            />
+            <FormHelperText>
+              건너 뛰려면 투표가 필요합니다
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={this.handleRoomButtonPressed}
+          >
+            방 만들기
+          </Button>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button color="secondary" variant="contained" to="/" component={Link}>
+            뒤로가기
+          </Button>
+        </Grid>
+      </Grid>
+    );
   }
-
 }
